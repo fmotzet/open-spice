b37d8e0c39c5efcda301f53fc13cb8e1b91a603e
fmotzet <felix.motzet@gmail.com>
2024-05-22 13:59:53 +0200

    init, basic server and client to test

---
+++ testclient.py
@@ -0,0 +1,60 @@
+import socket
+
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+
+import subprocess
+import json
+import pprint
+pp= pprint.PrettyPrinter(indent=4)
+
+softwareCommands = ["Get-ChildItem -Path HKLM:SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall | Get-ItemProperty | Sort-Object -Property DisplayName | Select-Object -Property DisplayName, DisplayVersion, InstallDate, InstallLocation | ConvertTo-Json",
+            "Get-ChildItem -Path HKCU:Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall | Get-ItemProperty | Sort-Object -Property DisplayName | Select-Object -Property DisplayName, DisplayVersion, InstallDate, InstallLocation | ConvertTo-Json",
+            "Get-ChildItem -Path HKLM:Software\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall | Get-ItemProperty | Sort-Object -Property DisplayName | Select-Object -Property DisplayName, DisplayVersion, InstallDate, InstallLocation | ConvertTo-Json",]
+computerNameCommand = "Get-ItemProperty HKLM:SYSTEM\\CurrentControlSet\\Control\\ComputerName\\ComputerName | Get-ItemProperty | Sort-Object | Select-Object -Property ComputerName | ConvertTo-Json"
+windowsInfoCommand = "Get-ItemProperty 'HKLM:SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion' | Get-ItemProperty | Sort-Object | Select-Object -Property CompositionEditionID, CurrentBuild, DisplayVersion, RegisteredOrganization, RegisteredOwner | ConvertTo-Json"
+def runAndConvert(command):
+    myp = subprocess.Popen(["powershell", command],shell=True, stdout=subprocess.PIPE)
+    myb = (myp.stdout.read()).decode('utf-8)')
+    myo = json.loads(myb)
+    return myo
+
+def collector():
+    allSoftware = []
+    for mycommand in softwareCommands:
+        for innerdict in runAndConvert(mycommand):
+            if innerdict["DisplayName"] == None:
+                continue
+            else:
+                allSoftware.append(innerdict)
+    thisComputerName = runAndConvert(computerNameCommand)
+    thisWinInfo = runAndConvert(windowsInfoCommand)
+    
+    finalDict = {thisComputerName['ComputerName']:[thisWinInfo,allSoftware]}
+    
+    #print (finalDict)
+    # print(type(allSoftware))
+    # pp.pprint(thisComputerName)
+    # pp.pprint(thisWinInfo)
+    # pp.pprint(allSoftware)
+    return(thisComputerName)
+
+if type(collector()) == dict or list:
+    myWinInfo = json.dumps(collector())
+else:
+    print("wrong type")
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    while True:
+        mystring = input("Enter something:")
+        if mystring == "you":
+            mystring = myWinInfo
+
+        s.sendall((mystring).encode("utf-8"))
+        data = s.recv(1024)
+
+        if data == (b"terminting connection"):
+            print(data)
+            break
+        #print(data)
---
+++ testserver.py
@@ -0,0 +1,42 @@
+import socket
+
+host = "127.0.0.1"
+port = 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as thisSocket:
+    print("""
+   ___                   ___      _         __      __       _       
+  / _ \\ _ __  ___ _ _   / __|_ __(_)__ ___  \\ \\    / /__ _ _| |__ ___
+ | (_) | '_ \\/ -_) ' \\  \\__ \\ '_ \\ / _/ -_)  \\ \\/\\/ / _ \\ '_| / /(_-<
+  \\___/| .__/\\___|_||_| |___/ .__/_\\__\\___|   \\_/\\_/\\___/_| |_\\_\\/__/
+       |_|                  |_|                                      
+          Welcome to Open Spice Works V0.1
+          """)
+    print("Waiting for connections...")
+    thisSocket.bind((host, port))
+    thisSocket.listen()
+    
+    while True:
+        try:
+            conn, addr = thisSocket.accept()
+            print(f"Connected by {addr}")            
+            with conn:
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    if data == b"bye":
+                        print("Received termination request, closing connection")
+                        conn.send(b"terminating connection")
+                        break
+                    elif data == b"kill":
+                        print("Received termination request, killing server")
+                        conn.send(b"terminating connection")
+                        thisSocket.close()
+                        exit(0)
+                    else:
+                        print(data)
+                        conn.sendall(b"received...")
+        except Exception as e:
+            print(f"An error occurred: {e}")     
+
